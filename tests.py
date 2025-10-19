9debe6b616cb37443ae6b95ae7f541430954a955
Sacha <sacha.tousignant@gmail.com>
2025-10-18 20:27:13 -0400

    Get file content function

--- tests.py
+++ tests.py
@@ -1,43 +1,26 @@
-#get_files_info("calculator", ".")
-#get_files_info("calculator", "pkg")
-#get_files_info("calculator", "/bin")
-#get_files_info("calculator", "../")
-
-
 import unittest
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content
 
 class TestMain(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_get_files_info_calculator_current_directory(self):
-        return_value = get_files_info("calculator", ".")
-        self.assertIn("main.py", return_value)
-        self.assertIn("tests.py", return_value)
-        self.assertIn("pkg", return_value)
+    def test_get_files_info_valid(self):
+        return_value = get_file_content("calculator", "main.py")
         print(return_value)
 
-    def test_get_files_info_calculator_valid_directory(self):
-        return_value = get_files_info("calculator", "pkg")
-        self.assertIn("calculator.py", return_value)
-        self.assertIn("render.py", return_value)
+    def test_get_files_info_valid2(self):
+        return_value = get_file_content("calculator", "pkg/calculator.py")
         print(return_value)
 
-    def test_get_files_info_not_permitted_outside_directory1(self):
-        return_value = get_files_info("calculator", "/bin")
+    def test_get_files_info_invalid_outside_directory(self):
+        return_value = get_file_content("calculator", "/bin/cat")
         self.assertIn("Error: Cannot list", return_value)
         print(return_value)
 
-    def test_get_files_info_not_permitted_outside_directory2(self):
-        return_value = get_files_info("calculator", "../")
-        self.assertIn("Error: Cannot list", return_value)
-        print(return_value)
-
-    def test_get_files_info_not_directory(self):
-        return_value = get_files_info("calculator", "main.py")
-        self.assertIn("Error:", return_value)
-        self.assertIn("is not a directory", return_value)
+    def test_get_files_info_invalid_file(self):
+        return_value = get_file_content("calculator", "pkg/does_not_exist.py")
+        self.assertIn("Error: File not found", return_value)
         print(return_value)
 
 if __name__ == "__main__":
